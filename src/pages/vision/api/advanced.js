f81d2be18184ae34d45e938b9d945c2ee233d692
Robin Wu <robin.qw.wu@mail.foxconn.com>
2019-01-31 16:32:35 +0800

    vision will post data to test server

--- src/pages/vision/api/advanced.js
+++ src/pages/vision/api/advanced.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
+import { getIpAddress } from './getIpAddress';
 
 function getAdvanced (params) {
   const currentUrl = window.location.hash;
   const hostname = window.location.hostname;
+  const ipAddress = getIpAddress();
   if (hostname.includes('localhost')) {
     return axios.post('http://localhost:8000/get_yield_throughput/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '192.168.75.128' });
   }
   if (currentUrl.includes('fortinet')) {
     console.log('it is fortinet');
-    return axios.post('http://10.132.45.164:8000/get_yield_throughput/',
+    return axios.post('http://' + ipAddress + '/get_yield_throughput/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '10.167.6.165' });
   } else {
     console.log('it is Vistion');
-    return axios.post('http://10.132.45.164:8000/get_yield_throughput/',
+    return axios.post('http://' + ipAddress + '/get_yield_throughput/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '192.168.200.214' });
   }
 }
@@ -22,17 +24,18 @@ function getAdvanced (params) {
 function getAnalysis (params) {
   const currentUrl = window.location.hash;
   const hostname = window.location.hostname;
+  const ipAddress = getIpAddress();
   if (hostname.includes('localhost')) {
     return axios.post('http://localhost:8000/get-failure-analysis/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '192.168.75.128' });
   }
   if (currentUrl.includes('fortinet')) {
     console.log('it is fortinet');
-    return axios.post('http://10.132.45.164:8000/get-failure-analysis/',
+    return axios.post('http://' + ipAddress + '/get-failure-analysis/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '10.167.6.165' });
   } else {
     console.log('it is Vistion');
-    return axios.post('http://10.132.45.164:8000/get-failure-analysis/',
+    return axios.post('http://' + ipAddress + '/get-failure-analysis/',
       { 'data_type': params.data_type, 'start_date': params.start_date, 'end_date': params.end_date, 'uuttype': params.uuttype, 'sernum': params.sernum, 'area': params.area, 'machine': params.machine, 'mode': params.mode, 'result': params.result, 'test': params.test, 'database': '192.168.200.214' });
   }
 }
---
+++ src/pages/vision/api/getIpAddress.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+function getIpAddress () {
+  let hostname = window.location.hash.split('=')[1];
+  if (!hostname) { 
+    hostname = window.location.hostname;
+  }
+  // console.log(hostname);
+  // hostname = '192.168.75.128';
+  // hostname = '10.167.219.25';
+  return hostname + ':8000';
+}
+
+export {
+  getIpAddress
+};
