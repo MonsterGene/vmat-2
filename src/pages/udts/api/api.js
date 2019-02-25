45ed4769b563ef0ff82c86cd949fdfdfffdc5970
robinwu <robinwu.qw@gmail.com>
2019-02-25 11:09:13 +0800

    remove UDTS

--- src/pages/udts/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-function getAPI (sernum) {
-  return axios.post('http://10.132.45.164:8000/get-record-by-sernum/', { 'sernum': sernum, 'database': '10.167.6.165' });
-}
-
-export {
-  getAPI
-};
--- src/pages/udts/menu.js
+++
@@ -1,9 +0,0 @@
-const udts_menu = [
-  {
-    title: 'UDTS',
-    icon: 'filter_vintage',
-    name: 'udts',
-  },
-];
-
-export default udts_menu;
