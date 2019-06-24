a2404a2053bd1c5185640d209ae1167d49becc8b
robinwu <robwu2@cisco.com>
2019-06-24 08:43:55 +0800

    update

---
+++ src/api/menus/gitLab.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Git Lab',
+  icon: 'filter_vintage',
+  target: '_blank',
+  href: 'http://10.132.45.143',
+};
--- src/api/menus/menu.js
+++ src/api/menus/menu.js
@@ -1,10 +1,12 @@
 import vision from '../../pages/vision/menu';
 import genius from '../../pages/genius/menu';
+import gitLab from './gitLab';
 
 const Menu = [];
 
 Menu.push(...genius);
 Menu.push(...vision);
+Menu.push(gitLab);
 // Menu.push(...example);
 
 export default Menu;
