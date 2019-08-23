67e5153078e4b1900eb0859de0930cc305d91781
F1325140 <robwu2@cisco.com>
2019-08-23 15:18:14 +0800

    update

--- src/pages/vision/api/getTestLog.js
+++ src/pages/vision/api/getTestLog.js
@@ -4,7 +4,7 @@ import { getIpAddress } from './getIpAddress';
 function getTestLogList (time_stamp, machine, container) {
   // const hostname = window.location.hostname;
   const ipAddress = getIpAddress();
-  return axios.get('http://' + ipAddress + '/get-test-log-list/?time_stamp=' + time_stamp + '&machine=' + machine + '&container=' + container);
+  return axios.get('http://' + ipAddress + '/get-test-log-list/?time_stamp=' + time_stamp + '&machine=' + machine + '&container=' + container + '&host=' + ipAddress.split(':')[0]);
 }
 
 export {
