0b2b12cbd4b8de9b9b76f2aaec148e1c51f5b0e3
F1325140 <robwu2@cisco.com>
2019-08-28 09:37:48 +0800

    update

--- src/pages/vision/api/getIpAddress.js
+++ src/pages/vision/api/getIpAddress.js
@@ -1,12 +1,4 @@
-function getIpAddress () {
-  let hostname = window.location.hash.split('=')[1];
-  if (!hostname) { 
-    hostname = window.location.hostname;
-  }
-  // console.log(hostname);
-  // hostname = '10.167.219.247';
-  return hostname + ':8000/api';
-}
+import { getIpAddress } from '../../genius/api/basic';
 
 export {
   getIpAddress
