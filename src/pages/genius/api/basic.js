0ccd429add6026599e14f1cf3bc70e7c3510a53f
robinwu <robwu2@cisco.com>
2019-10-29 14:10:38 +0800

    update

--- src/pages/genius/api/basic.js
+++ src/pages/genius/api/basic.js
@@ -5,7 +5,7 @@ function getIpAddress () {
   }
   // console.log(hostname);
   // hostname = '10.167.219.247';
-  hostname = '10.167.219.100';
+  // hostname = '10.167.219.100';
   // hostname = '10.167.16.57';
   // return 'http://' + hostname + ':8000';
   return 'https://' + hostname + ':8000/api';
